88c7838a99305d7359be0bcbd1f9c6eab3a4ca7f
MikulikV <mikulik1995@outlook.com>
2022-12-15 15:16:58 +0300

    Added new component Dialogs and set Navigation in Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,16 +1,30 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import s from './Navbar.module.css';
 
-console.log(s);
+
+const setClass = ({isActive}) => isActive ? s.activeLink : s.item;
+
+
 const Navbar = () => {
   return (
     <nav className={s.nav}>
       <ul>
-        <li><a className={`${s.item} ${s.active}`} href='#s'>Profile</a></li>
-        <li><a className={s.item} href='#s'>Messages</a></li>
-        <li><a className={s.item} href='#s'>News</a></li>
-        <li><a className={s.item} href='#s'>Music</a></li>
-        <li><a className={s.item} href='#s'>Settings</a></li>
+        <li className={s.item}>
+          <NavLink to='/profile' className={setClass}>Profile</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to='/dialogs' className={setClass}>Messages</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to='/news' className={setClass}>News</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to='/music' className={setClass}>Music</NavLink>
+        </li>
+        <li className={s.item}>
+          <NavLink to='/settings' className={setClass}>Settings</NavLink>
+        </li>
       </ul>
     </nav>
   );
